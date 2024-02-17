d2e217684261bf8d2c3c29d5c46d3df93d6a0ff8
KakoFujimoto <ayaka20111005@yahoo.co.jp>
2024-02-17 09:21:16 +0900

    タイトルと価格だけをスクレイピングして配列に格納するようにした

--- index.js
+++ index.js
@@ -14,17 +14,20 @@ const URL = "https://search.rakuten.co.jp/search/mall/keyboard/";
 axios.get(URL)
 .then((response) =>{
     const htmlParser = response.data;
-    console.log(htmlParser);
+    // console.log(htmlParser);
 
     const $ = cheerio.load(htmlParser);
 
     $(".searchresultitem" ,htmlParser).each(function(){
-        const title = $(this).find(".title").text();
-        console.log(title);
-    })
-    .catch((error) => {
+        const title = $(this).find(".title--zfJkV").text();
+        const price = $(this).find(".price--OX_YW").text();
+        data.push({title , price});
+        console.log(data);
+    });
+})
+.catch((error) => {
         console.error("Error fetching data:", error); // エラーが発生した場合にログに出力する
     });
-});
+
 
 app.listen(PORT, console.log("surver is running!"));
